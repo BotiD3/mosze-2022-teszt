0af6043e39f603d0bd37519083d37cf8be60559c
BotiD3 <nagybotond123@gmail.com>
2022-09-09 16:19:45 +0200

    errors commented

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; //sintax error
+    std::cout << '1-100 ertekek duplazasa' //missing code
+    for (int i = 0;) //wrong for sintax
     {
-        b[i] = i * 2;
+        b[i] = i * 2; //logic error
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++) //missing code
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" //missing code
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;
     for (int i = 0; i < N_ELEMENTS, i++)
     {
-        atlag += b[i]
+        atlag += b[i] //missing comma
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
